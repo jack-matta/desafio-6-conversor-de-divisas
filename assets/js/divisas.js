8079acf2c4f01b555870718eb1f960bf17c5fbd2
jack-matta <111831247+jack-matta@users.noreply.github.com>
2024-04-09 21:51:00 -0400

    desafio 6

---
+++ assets/js/divisas.js
@@ -0,0 +1,67 @@
+const input = document.querySelector('.input');
+const select = document.querySelector('.select');
+const button = document.querySelector('.buscar');
+const span = document.querySelector('.resultado');
+const canvas = document.querySelector('.grafico');
+
+const url = "https://mindicador.cl/api";
+
+const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    return ` ${day}/${month}/${year}`
+}
+
+let myChart = null;
+
+function renderGrafico(data) {
+    console.log(data)
+    const config = {
+        type: "line",
+        data: {
+            labels: data.map((elem) => formatDate(new Date(elem.fecha))
+            ),
+            datasets: [{
+                label: "Ultimos 10 días",
+                backgroundColor: "blue",
+                data: data.map((elem) => elem.valor
+                ),
+            }]
+        }
+    }
+    canvas.style.backgroundColor = "white";
+    if (myChart) {
+        myChart.destroy();
+    }
+    myChart = new Chart(canvas, config);
+}
+async function buscarCotizacion() {
+    try {
+        
+        const moneda = select.value;
+        const fetching = await fetch(`${url}/${moneda}`);
+        const data = await fetching.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+        span.innerHTML = "Error... vuelva a intentar";
+    }
+}
+
+button.addEventListener('click', async () => {
+    span.innerHTML = `Cargando...`
+
+    const result = await buscarCotizacion()
+    const serie = result.serie;
+    const cantidad = input.value;
+    const lastValue = serie[0].valor;
+    const calculo = cantidad / lastValue;  
+    
+    const valor = (calculo).toFixed(2);
+    const data = serie.slice(0, 10).reverse();
+
+    span.innerHTML = `La Contización del día es $${valor} `
+    renderGrafico(data);
+})
+
